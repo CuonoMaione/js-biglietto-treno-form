b4bdfb81ec17cc21474c6b7336d607503df03e5d
CuonoMaione <cuonomaione99@outlook.it>
2023-05-16 09:39:55 +0200

    fix click function

--- js/myscript.js
+++ js/myscript.js
@@ -1,28 +1,27 @@
-
-const userAge = document.getElementById("Age")
-
-const userKm = document.getElementById("distance")
-
 const OKay = document.getElementById('button');
 
-let Price = (userKm.value * 0.21 );
-
-let result;
-
-if(userAge.value <= 17 ){
-    result = parseFloat(Price - [(Price * 20) /100]).toFixed(2) ;
-} else if (userAge.value > 65) {
-    result = parseFloat(Price - [(Price * 40) /100]).toFixed(2) ;
-} else {
-    result = parseFloat(Price).toFixed(2) ;
-};
-
-
-
-document.getElementById("distance").innerHTML += userKm.value + " Km";
-document.getElementById("Age").innerHTML += userAge + " anni";
-
 OKay.addEventListener('click', function() {
+    
+    const userAge = document.getElementById("Age")
+
+    const userKm = document.getElementById("distance")
+
+    
+
+    let Price = (userKm.value * 0.21 );
+
+    let result;
+
+    document.getElementById("distance").innerHTML += userKm.value + " Km";
+    document.getElementById("Age").innerHTML += userAge + " anni";
+
+    if(userAge.value <= 17 ){
+        result = parseFloat(Price - [(Price * 20) /100]).toFixed(2) ;
+    } else if (userAge.value > 65) {
+        result = parseFloat(Price - [(Price * 40) /100]).toFixed(2) ;
+    } else {
+        result = parseFloat(Price).toFixed(2) ;
+    };
     
     document.getElementById("price").innerHTML = result + "£";
 
