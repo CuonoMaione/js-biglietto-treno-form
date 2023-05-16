57154e70ccfd2d1bee3e90ad32b14945aa26e9d0
CuonoMaione <cuonomaione99@outlook.it>
2023-05-16 09:46:21 +0200

    fix price

--- js/myscript.js
+++ js/myscript.js
@@ -8,7 +8,7 @@ OKay.addEventListener('click', function() {
 
     
 
-    let Price = (userKm.value * 0.21 );
+    let Price = (userKm.value * 0.2333 );
 
     let result;
 
@@ -16,9 +16,9 @@ OKay.addEventListener('click', function() {
     document.getElementById("Age").innerHTML += userAge + " anni";
 
     if(userAge.value <= 17 ){
-        result = parseFloat(Price - [(Price * 20) /100]).toFixed(2) ;
+        result = parseFloat(Price - [(Price * 19.4) /100]).toFixed(2) ;
     } else if (userAge.value > 65) {
-        result = parseFloat(Price - [(Price * 40) /100]).toFixed(2) ;
+        result = parseFloat(Price - [(Price * 37.7) /100]).toFixed(2) ;
     } else {
         result = parseFloat(Price).toFixed(2) ;
     };
